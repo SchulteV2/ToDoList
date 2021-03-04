3cdd1b9f0ac06b117a9760e43d3b780c1211392e
Vinicius Schulte <schultev2@gmail.com>
2021-03-04 20:43:52 -0300

    Criado botão que adiciona tarefas aleatórias, mostrando o id, e a data de criação

--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, Button, FlatList } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import TaskItem from '../components/TaskItem';
 
 const TaskScreen = () => {
-    const tarefas = [
-        { id: '1', description: "Gravar video aula", date: "20/03/2021" },
-        { id: '2', description: "Gravar video aula 2", date: "21/03/2021" },
-        { id: '3', description: "Gravar video aula 3", date: "22/03/2021" },
-        { id: '4', description: "Gravar video aula 4", date: "23/03/2021" },
-    ]
+    
+    let [tarefas, setTarefas] = useState([])
 
     return (
-        <FlatList
-            style={styles.taskList}
-            keyExtractor={(task) => task.id}
-            data={tarefas}
-            renderItem={(element) => {
-                return <TaskItem task={element.item} />
-            }}
-        />
+        <View>
+            <TouchableOpacity style={styles.taskBtn} onPress={() => {
+                setTarefas([...tarefas, newTask()])
+            }}>
+                <Text>Gerar Tarefa</Text>
+            </TouchableOpacity>
+            <FlatList
+                style={styles.taskList}
+                keyExtractor={(task) => task.id}
+                data={tarefas}
+                renderItem={(element) => {
+                    return <TaskItem task={element.item} />
+                }}
+            />
+        </View>
     )
 };
+
+const newTask = () => {
+    let id = Math.random();
+    let date = new Date();
+    
+    let day = date.getDay();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    return { id: `${id}`, description: `Tarefa #${id}`, date: `${day}/${month}/${year}` }
+}
 
 const styles = StyleSheet.create({
     taskList: {
         marginTop: 20
+    },
+    taskBtn: {
+        marginTop: 10,
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10
     }
 })
 
