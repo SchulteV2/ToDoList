cd742dae5da5f38acbcc3d5424b59b3d4f53671a
Vinicius Schulte <schultev2@gmail.com>
2021-03-31 12:29:06 -0300

    Modificado a listagem das task para funcionar como List do react-native-paper, adicionado botão usando FontAwesome e AntDesign

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,45 +1,28 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { List } from 'react-native-paper';
+import { FontAwesome, AntDesign } from '@expo/vector-icons';
 
 const TaskItem = (props) => {
 
     return (
-        <View style={styles.container}>
-            <Image style={styles.imageColumn} source={ require('../../assets/task.png') } />
-            <Text style={styles.descColumn}>{props.task.description}</Text>
-            <Text style={styles.dateColumn}>{props.task.date}</Text>
-            <TouchableOpacity style={styles.removeColumn} onPress={() => {
-                props.removeTask(props.task.id)
-            }}>
-                <Text style={{color: '#FFFFFF'}}>X</Text>
-            </TouchableOpacity>
-        </View>
+        <List.Item
+                title={props.task.date}
+                description={props.task.description}
+                left={(paperProps) => <FontAwesome {...paperProps} style={styles.icon} name="tasks" size={24} color="black" />}
+                right={(paperProps) => 
+                    <TouchableOpacity  onPress={() => {props.removeTask(props.task.id)}}>
+                        <AntDesign {...paperProps} style={styles.icon} name="closesquare" size={24} color="red" />
+                    </TouchableOpacity>
+                }
+            />
     )
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'flex-start',
-        marginTop: 20
-    },
-    imageColumn: {
-        width: '10%',
-        resizeMode: 'contain'
-    },
-    descColumn: {
-        width: '60%'
-    },
-    dateColumn: {
-        width: '25%'
-    },
-    removeColumn: {
-        // width: '5%',
-        alignItems: 'center',
-        backgroundColor: '#FF0000'
+    icon: {
+        marginTop: 17
     }
 })
 
