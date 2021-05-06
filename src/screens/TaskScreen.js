a170a58e3d34df3fad03692320f4671d5ef87a42
Vinicius Schulte <schultev2@gmail.com>
2021-05-06 17:21:41 -0300

    Conexão com o Firebase, com add e remove do db

--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -3,27 +3,62 @@ import { StyleSheet, View, FlatList } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import TaskItem from '../components/TaskItem';
 
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD20zO5vAWTXX0Jgqfq84rpx5GJVt4UPb8",
+    authDomain: "todolist-766e4.firebaseapp.com",
+    projectId: "todolist-766e4",
+    storageBucket: "todolist-766e4.appspot.com",
+    messagingSenderId: "491965912404",
+    appId: "1:491965912404:web:44866f280a382420fb64c1",
+    measurementId: "G-YTBX147P3V"
+  };
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+
 const TaskScreen = () => {
     
     let [tarefas, setTarefas] = useState([])
     let [description, setDescription] = useState('')
+
+    getTasks()
     
     const removeTask = (id) => {
-        let filteredList = tarefas.filter((task) => task.id != id);
-        setTarefas(filteredList);
+        db.collection('tasks').doc(id).delete()
+        getTasks()
     }
     
-    const newTask = () => {
-        let id = Math.random();
-        let date = new Date();
+    async function getTasks() {
+        const tasksRef = db.collection('tasks')
+        const snapshot = await tasksRef.get()
     
+        let tasks = snapshot.docs.map(doc => {
+            return task = {id: doc.id, ...doc.data()}
+        })
+
+        setTarefas(tasks)
+    }
+
+    async function newTask() {
+        let date = new Date();
         let day = date.getDay();
         let month = date.getMonth() + 1;
         let year = date.getFullYear();
 
+        let formattedDate = `${day}/${month}/${year}`
+        
         setDescription('')
-    
-        return { id: `${id}`, description: description, date: `${day}/${month}/${year}` }
+
+        const res = await db.collection("tasks").add({
+            date: formattedDate,
+            description: description
+        })
+        
+        return { id: res.id, description: description, date: formattedDate }
     }
 
     return (
